4a77d134ff8d24665e46c022a82b62c400c77d56
sohaehrari <sohaehraari@gmail.com>
2026-09-10 15:55:12 +0430

    Connect history component to history page

---
+++ app/history/page.js
@@ -0,0 +1,11 @@
+import History from "@/components/history/History";
+
+export default function historyPage(){
+    return(
+        <main className="min-h-screen bg-zinc-950 py-10 text-white sm:px-6 lg:px-8">
+<div>
+    <History/>
+</div>
+        </main>
+    )
+}
