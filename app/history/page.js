ccbd0925346e783a67bafde7e345c0a47e4fe54c
sohaehrari <sohaehraari@gmail.com>
2026-09-10 16:31:29 +0430

    load persisted history on history page

--- app/history/page.js
+++ app/history/page.js
@@ -1,10 +1,16 @@
+"use client"
+
+
+
 import History from "@/components/history/History";
+import useLocalStorage from "@/hooks/useLocalStorage";
 
 export default function historyPage(){
+    const[history]=useLocalStorage("nexacalc-history",[])
     return(
         <main className="min-h-screen bg-zinc-950 py-10 text-white sm:px-6 lg:px-8">
 <div>
-    <History/>
+    <History history={history}/>
 </div>
         </main>
     )
