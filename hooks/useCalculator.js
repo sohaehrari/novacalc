7a5068421961d3f638b2d2e50d233e4dad0d2a9c
sohaehrari <sohaehraari@gmail.com>
2026-09-10 16:25:30 +0430

    persist calculator history in local storage

--- hooks/useCalculator.js
+++ hooks/useCalculator.js
@@ -1,54 +1,70 @@
-import { useReducer } from "react";
+"use client";
+
+import { useEffect, useReducer } from "react";
 import calculatorReducer from "@/reducers/calculatorReducer";
+import useLocalStorage from "./useLocalStorage";
 
-export default function useCalculator(){
-    const[state,dispatch]=useReducer(calculatorReducer,{
-        expression:"",
-        result:"0",
-        memory:"0",
-        history:[],
-    })
+export default function useCalculator() {
+  const [state, dispatch] = useReducer(calculatorReducer, {
+    expression: "",
+    result: "0",
+    memory: 0,
+    history: [],
+  });
 
+  const [savedHistory, setSavedHistory] = useLocalStorage(
+    "nexacalc-history",
+    []
+  );
 
-    const inputNumber=(number)=>{
-        dispatch({
-            type:"INPUT_NUMBER",
-            payload:number
-        });
+  // Connects calculator calculations with persistent history stored in the browser
+  useEffect(() => {
+    if (state.history.length > savedHistory.length) {
+      setSavedHistory(state.history);
     }
+  }, [state.history, savedHistory.length, setSavedHistory]);
 
-    const inputOperator=(operator)=>{
-        dispatch({
-            type:"INPUT_OPERATOR",
-            payload:operator
-        });
-    }
+  const inputNumber = (number) => {
+    dispatch({
+      type: "INPUT_NUMBER",
+      payload: number,
+    });
+  };
 
-    const calculate=()=>{
-        dispatch({
-            type:"CALCULATE",
-        });
-    }
+  const inputOperator = (operator) => {
+    dispatch({
+      type: "INPUT_OPERATOR",
+      payload: operator,
+    });
+  };
 
+  const calculate = () => {
+    dispatch({
+      type: "CALCULATE",
+    });
+  };
 
-    const clear=()=>{
-        dispatch({
-            type:"CLEAR"
-        })
-    }
-    const backspace=()=>{
-        dispatch({
-            type:"BACKSPACE"
-        })
-    }
+  const clear = () => {
+    dispatch({
+      type: "CLEAR",
+    });
+  };
 
+  const backspace = () => {
+    dispatch({
+      type: "BACKSPACE",
+    });
+  };
 
-    return{
-        state,
-        clear,
-        backspace,
-        inputNumber,
-        inputOperator,
-        calculate,
-    }
+  return {
+    state: {
+      ...state,
+      history: savedHistory,
+    },
+    inputNumber,
+    inputOperator,
+    calculate,
+    clear,
+    backspace,
+  };
 }
